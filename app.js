c88492260a804d305600011966403f91ed52d0df
Jaloliddin <oficialjaloliddin@gmail.com>
2024-07-26 15:54:14 +0500

    express app

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const express = require("express")
+const express = require("express");
 
-const app = express()
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message:'Hello Bro'})
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Hello posts");
+});
 
 const PORT = 8080
 
